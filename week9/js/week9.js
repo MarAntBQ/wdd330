28e9f974216761f1b486637f3ae1468096147ca9
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-12 23:18:52 -0500

    I'm done with this week practices!

--- week9/js/week9.js
+++ week9/js/week9.js
@@ -131,3 +131,65 @@ function factorsOf(n) {
     }
     return factors.sort((a, b) => a - b);
 }
+
+function Notifications1F() {
+    let permission = Notification.permission;
+    if (permission === "granted") {
+        showNotification();
+    } else if (permission === "default") {
+        requestAndShowPermission();
+    } else {
+        alert("Use normal alert");
+    }
+}
+
+function showNotification() {
+    if (document.visibilityState === "visible") {
+        return;
+    }
+    var title = "JavaScript Jeep";
+    var body = "Message to be displayed";
+    var notification = new Notification('Title', {
+        body,
+        icon
+    });
+    notification.onclick = () => {
+        notification.close();
+        window.parent.focus();
+    }
+}
+
+function requestAndShowPermission() {
+    Notification.requestPermission(function (permission) {
+        if (permission === "granted") {
+            showNotification();
+        }
+    });
+}
+
+function DranwCF() {
+    const canvasElement = document.getElementById('canvas');
+    const context = canvasElement.getContext('2d');
+    context.fillStyle = "#0000cc"; // a blue fill color
+    context.strokeStyle = "#ccc"; // a gray stroke color
+    context.lineWidth = 4;
+    context.fillRect(10, 10, 100, 50);
+    context.strokeRect(10, 100, 100, 50);
+    context.beginPath();
+    context.moveTo(130, 50);
+    context.lineTo(180, 50);
+    context.moveTo(155, 50);
+    context.lineTo(155, 90);
+    context.strokeStyle = '#c00';
+    context.lineWidth = 15;
+    context.stroke();
+    context.beginPath();
+    context.arc(200, 200, 30, 0, Math.PI * 2, false);
+    context.strokeStyle = '#ff0';
+    context.lineWidth = 4;
+    context.stroke();
+    context.fillStyle = '#0c0'; // a blue fill color
+    context.font = 'bold 26px sans-serif';
+    context.fillText('Hello', 20, 200);
+
+}
