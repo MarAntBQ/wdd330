821bbd5fc1296f79b5c5e71ad134658fe2ff7152
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-11-12 19:52:04 -0500

    Adding more examples!

--- week9/js/week9.js
+++ week9/js/week9.js
@@ -103,3 +103,8 @@ function GeolocationF() {
         window.alert(`Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`);
         } 
 }
+
+function FactorizorF() {
+    const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+    document.body.style.background = rainbow[Math.floor(7* Math.random())];
+}
